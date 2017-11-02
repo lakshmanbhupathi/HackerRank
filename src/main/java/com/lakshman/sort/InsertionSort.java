7ba367b74b564a726e28c963cc03bb5636b1c852
Lakshman <lakshmanbhupathi@gmail.com>
2017-11-02 23:39:42 +0530

    master: Added Insertion Sort

---
+++ src/main/java/com/lakshman/sort/InsertionSort.java
@@ -0,0 +1,34 @@
+package com.lakshman.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by Lakshman on 02-11-2017.
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(insertionSort(new int[]{5,1,2,3,6,9,8,7,4})));
+    }
+
+    private static int[] insertionSort(int[] array) {
+        int key,temp;
+
+        //*** loop 1 to array.length
+        for(int i =1 ; i< array.length ; i++){
+            key = array[i];
+
+            for(int j = i-1; j >= 0 ; j--) {
+
+                if(key < array[j]){
+
+                    // *** swap j and j+1
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
