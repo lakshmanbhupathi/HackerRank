1e2e4938fc709f1b885c1cc0df370d7790ff9142
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-05-17 23:28:28 +0530

    master: LookAndSayProblem

---
+++ src/main/java/com/lakshman/hackerrank/ctc/LookAndSayProblem.java
@@ -0,0 +1,53 @@
+package com.lakshman.hackerrank.ctc;
+
+/**
+ * Look and say
+ *
+ * Implement a function that reads off the digits of the input:
+ *
+ * lookAndSay(1) = 11 because it reads off 'one 1'
+ *
+ * lookAndSay(11) = 21 because it reads off 'two 1'
+ *
+ * lookAndSay(21) = 1211 because it reads off 'one 2, one 1'
+ *
+ * lookAndSay(digits, n) n times recursion of lookAndSay
+ *
+ *
+ */
+public class LookAndSayProblem {
+
+    static String lookAndSay(String start, int n) {
+
+        String value = start;
+        for (int i = 0; i < n; i++) {
+            value = lookAndSay(value);
+        }
+
+        return value;
+
+    }
+
+    static String lookAndSay(final String num) {
+        if (num == null || num.isEmpty()) {
+            return "";
+        }
+        int firstCharPos = 0;
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(firstCharPos) != num.charAt(i)) {
+                String digitsFound = num.substring(firstCharPos, i);
+                builder.append(digitsFound.length()).append(num.charAt(firstCharPos));
+                firstCharPos = i;
+            }
+        }
+        builder.append(num.substring(firstCharPos, num.length()).length()).append(num.charAt(firstCharPos));
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lookAndSay("11"));
+    }
+
+
+}
