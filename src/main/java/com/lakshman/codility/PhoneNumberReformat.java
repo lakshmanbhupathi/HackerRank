7c7e38363bb241775c05d6449a053cb8a1c84240
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-06-10 23:43:30 +0530

    master: Added PhoneNumberReformat.java

--- src/main/java/com/lakshman/codility/PhoneNumberReformat.java
+++ src/main/java/com/lakshman/codility/PhoneNumberReformat.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayDeque;
-import java.util.Queue;
 
 /**
  * Codility Test
@@ -14,10 +13,10 @@ import java.util.Queue;
 public class PhoneNumberReformat {
 
     @Test
-    public void test(){
-        Assert.assertEquals("004-448-555-583-61",solution("00-44  48 5555 8361"));
-        Assert.assertEquals("022-198-53-24",solution("0 - 22 1985--324"));
-        Assert.assertEquals("555-372-654",solution("555372654"));
+    public void test() {
+        Assert.assertEquals("004-448-555-583-61", solution("00-44  48 5555 8361"));
+        Assert.assertEquals("022-198-53-24", solution("0 - 22 1985--324"));
+        Assert.assertEquals("555-372-654", solution("555372654"));
     }
 
     public String solution(String S) {
@@ -28,28 +27,29 @@ public class PhoneNumberReformat {
             try {
                 Integer i = Integer.parseInt(c + "");
                 queue.addLast(i);
-            }catch (Exception e){
+            } catch (Exception e) {
                 continue;
             }
         }
 
         int currentCounter = 0;
-        while (!queue.isEmpty()){
-            if(queue.size() == 4 && currentCounter == 0){
+        while (!queue.isEmpty()) {
+            if (queue.size() == 4 && currentCounter == 0) {
                 sb.append(queue.removeFirst());
                 sb.append(queue.removeFirst());
                 sb.append("-");
 
-            } if(queue.size() == 2 && currentCounter == 0){
+            }
+            if (queue.size() == 2 && currentCounter == 0) {
                 sb.append(queue.removeFirst());
                 sb.append(queue.removeFirst());
-            } else{
+            } else {
                 sb.append(queue.removeFirst());
                 currentCounter++;
 
-                if (currentCounter==3 && !queue.isEmpty()){
+                if (currentCounter == 3 && !queue.isEmpty()) {
                     sb.append("-");
-                    currentCounter=0;
+                    currentCounter = 0;
                 }
             }
         }
