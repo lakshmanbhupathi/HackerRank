9568931ac129be0404ec61f7e9383ebb362f04c7
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-06-22 02:08:07 +0530

    master: Added ShuffleDigits.java

---
+++ src/main/java/com/lakshman/codility/ShuffleDigits.java
@@ -0,0 +1,29 @@
+package com.lakshman.codility;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShuffleDigits {
+    @Test
+    public void test() {
+        Assert.assertEquals(162534,solution(123456));
+        Assert.assertEquals(103,solution(130));
+    }
+
+    // TODO mathematical solution
+    public int solution(int A) {
+        char[] charArray = (A+"").toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0, j = charArray.length -1 ; i < j ; ) {
+            sb.append(charArray[i++]);
+            sb.append(charArray[j--]);
+            if(i == j){
+                sb.append(charArray[i]);
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+
+}
